1200368212ba992b02e5a91d34245b186e66b686
Sanjay Rastogi <sanjayrastogi92@gmail.com>
2020-12-25 17:09:18 +0530

    Adding ReverseWord file

---
+++ src/dec_25/ReverseWord.java
@@ -0,0 +1,21 @@
+package dec_25;
+
+import java.util.*;
+
+
+public class ReverseWord{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string to reverse word");
+		String str = sc.nextLine();
+		
+		String tokens[] = str.split(" ");
+		int tokenLen = tokens.length;
+		
+		for(int i=tokenLen-1; i>=0; i--) {
+			System.out.print(tokens[i]+" ");
+		}
+	}
+}
